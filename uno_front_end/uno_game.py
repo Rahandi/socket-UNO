6eee6fe0051c51c81781aa4ddbfb32ac1982c34d
Fadli Wildan <fwildanf@gmail.com>
2019-05-10 21:13:51 +0700

    Page Transition
    page transition from login -> start/waiting -> game

---
+++ uno_front_end/uno_game.py
@@ -0,0 +1,38 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.config import Config
+from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+
+class LoginPage(Screen):
+    user_id = 1
+    def change_screen(self, *args):
+        if self.user_id == 1:
+            App.get_running_app().root.current = "start_page"
+        else:
+            App.get_running_app().root.current = "waiting_page"
+
+class WaitingPage(Screen):
+    pass
+
+class StartPage(Screen):
+    pass
+
+class GamePage(Screen):
+    pass
+
+class ScreenManagement(ScreenManager):
+    pass
+
+
+# Config.set('kivy','window_icon','path/to/icon.ico')
+
+game = Builder.load_file("uno.kv")
+
+class MainGame(App):
+    def build(self):
+        return game
+
+if __name__ == "__main__":
+    MainGame().run()
